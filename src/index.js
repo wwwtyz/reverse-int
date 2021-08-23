60b80dfe27d091e42ff2e305c0dbfda3eaac3c35
wwwtyz <wwwtyz@tut.by>
2021-08-23 16:37:01 +0300

    feat: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n >= 0) {
+        result = Number(String(n).split('').reverse().join(''));
+
+    } else {
+        n = Math.abs(n)
+        result = (Number(String(n).split('').reverse().join('')));
+    }
+return result;
+};
